ee9726a6eb8318edca6c5ba16b6ed89c0ef419ec
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-23 23:43:30 +0530

    initiate virtual envrionment

--- generate.sh
+++ generate.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 # $1 => project name
+#!/bin/bash
+
+# Check if virtual environment is already activated
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Deactivating virtual environment..."
+    deactivate
+fi
+
+# Create a new virtual environment
+echo "Creating virtual environment..."
+python3 -m venv env
+source env/bin/activate
+
+# Install Django
+echo "Installing Django..."
+pip3 install django
+
+# Create a new Django project
+echo "Creating Django project..."
+mkdir $1
+cd $1
+mkdir $1
+# NAME => $1
